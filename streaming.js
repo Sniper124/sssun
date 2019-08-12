fabadebd93ba1763a6baa04bd6b30b055963e468
Sniper124 <54014910+Sniper124@users.noreply.github.com>
2019-08-12 07:41:33 +0200

    Update streaming.js

--- streaming.js
+++ streaming.js
@@ -11,7 +11,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "ID حقك") return;
+  if (message.author.id !== "605841757187604486") return;
 
   
   if (message.content.startsWith(prefix + 'setwatch')) {
